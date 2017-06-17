4db73fbd7d94e25daa74a2ed9f60b7aebac0422a
Tori Boone <vkishman88@gmail.com>
2017-06-16 20:59:13 -0400

    added code file

---
+++ Reverse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse
+{
+    class Program
+    {
+        public static int ReverseInt(int x)
+        {
+            string num = new String(x.ToString().Reverse().ToArray());
+            return int.Parse(num);
+
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(ReverseInt(num1));
+            Console.ReadLine();
+        }
+    }
+}
